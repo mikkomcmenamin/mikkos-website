d9d09e78a97f552b906aec31570614baed0ce22b
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2020-01-19 16:12:29 +0200

    Updates

---
+++ precache-manifest.a7c5f8e4482642ead115dcf93e6cd129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a02fda4e05b7174367d67653dc81c3",
+    "url": "/mikkos-website/index.html"
+  },
+  {
+    "revision": "7ddf0bcebf0f75dba59a",
+    "url": "/mikkos-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2ad6ef751de063d2dc25",
+    "url": "/mikkos-website/static/js/2.5ff0a3fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mikkos-website/static/js/2.5ff0a3fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ddf0bcebf0f75dba59a",
+    "url": "/mikkos-website/static/js/main.fca8fc75.chunk.js"
+  },
+  {
+    "revision": "ac9a2901c2c86310ff4a",
+    "url": "/mikkos-website/static/js/runtime-main.4a68a418.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mikkos-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
